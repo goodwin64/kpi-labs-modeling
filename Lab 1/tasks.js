1717de51e806b151f71ec5c9b00a8b109b1f79c3
Max Donchenko <wereya2@yandex.ru>
2017-12-28 22:14:56 +0200

    Lab 1 - task 3

--- Lab 1/tasks.js
+++ Lab 1/tasks.js
@@ -65,3 +65,35 @@ function task2(size, sigma, a, chartContainerId = 'chartContainer2') {
         chart.render();
     })();
 }
+
+function task3(size, a, c, chartContainerId = 'chartContainer3') {
+    let z = 1;
+    const data = new Array(size)
+        .fill(0)
+        .reduce(acc => {
+            z = (a * z) % c;
+            return acc.concat(z / c);
+        }, [])
+        .sort((a, b) => a - b);
+
+    (function renderChart() {
+        const dataPoints = getDataPoints(data);
+        const chart = new CanvasJS.Chart(chartContainerId, {
+            animationEnabled: true,
+            exportEnabled: true,
+            zoomEnabled: true,
+            theme: "light1", // "light1", "light2", "dark1", "dark2"
+            title: {
+                text: "Task 3"
+            },
+            data: [{
+                type: "column", //change type to bar, line, area, pie, etc
+                indexLabel: "{y}", //Shows y value on all Data Points
+                indexLabelFontColor: "#5A5757",
+                indexLabelPlacement: "outside",
+                dataPoints,
+            }]
+        });
+        chart.render();
+    })();
+}
